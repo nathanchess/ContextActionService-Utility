af750475f214a9cfa0a12115ee0303176798ba3c
Nathan Che <59159552+nathanchess@users.noreply.github.com>
2022-03-15 22:16:42 -0700

    Update PlayerCore.lua

--- StarterPlayerScripts/PlayerCore.lua
+++ StarterPlayerScripts/PlayerCore.lua
@@ -1,5 +1,5 @@
 -- || MODULES || --
-local CASUtility = require(game.ReplicatedStorage.Modules.Utils["CAS-Utility"])
+local CASUtility = require(game.ReplicatedStorage.Modules.Utils["CAS-Utility"]) -- Change path to module.
 
 -- || INITIALIZATION PHASE || --
 CASUtility.QuickBind()
